e9674241f49ab2803d6de9865b12bb68e5d43d94
moustafa3laa2 <moustafaalaa801@gmail.com>
2023-12-07 01:09:03 +0200

    trigger add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -13,8 +13,9 @@ class AddNoteCubit extends Cubit<AddNoteState> {
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNotesBox);
-      emit(AddNoteSuccess());
+
       await noteBox.add(note);
+      emit(AddNoteSuccess());
     } catch (e) {
       emit(
         AddNoteFailuer(
